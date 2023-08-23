6a9fa1a69d6e03c087c7544b8cdc5c593727c2c8
Hrtoimukra <rohitgkv110@gmail.com>
2023-08-23 19:40:42 +0530

    Add files via upload

---
+++ Base64_Encoding_System/Client.cpp
@@ -0,0 +1,338 @@
+
+#include <iostream>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+
+using namespace std;
+
+#define BUFFERSIZE 1024
+#define ACK "Message Received"
+#define TYPE1 '1'
+#define TYPE2 '2'
+#define TYPE3 '3'
+#define WAIT_TIME 5  // in seconds
+#define TIMEOUT 3000 // in milli seconds
+
+enum ErrorCode
+{
+    HOST_NAME_AND_PORT_NO_INVALID,
+    SOCKET_CREATION_FAILED,
+    CONNECTION_FAILED,
+    HOST_NOT_VALID,
+    ERROR_SENDING_MSG,
+    ERROR_READING_MSG,
+    PORT_NO_INVALID
+};
+
+enum ClosingState
+{
+    FIN_WAIT_1,
+    FIN_WAIT_2
+};
+
+void clientProcess(int, struct hostent *);
+void doClientServerCommunications(int);
+void closeConnectionToServer(int);
+void doBase64Encoding(char *, char *);
+void error(ErrorCode);
+void printToConsole(string);
+
+int main(int argc, char **argv)
+{
+    int portno;
+    struct hostent *server;
+
+    if (argc < 3)
+    {
+        error(HOST_NAME_AND_PORT_NO_INVALID);
+    }
+
+    portno = atoi(argv[2]);          // getting port no from CLI argument
+    server = gethostbyname(argv[1]); // getting server hostname
+
+    if (portno < 0 || portno > 65534)
+    {
+        error(PORT_NO_INVALID);
+    }
+
+    if (server == NULL)
+    {
+        error(HOST_NOT_VALID);
+    }
+
+    clientProcess(portno, server);
+
+    return 0;
+}
+
+/**
+ * @brief Error handling utility fuction
+ * @param errCode the error code for which exception need to be handled
+ */
+void error(ErrorCode errCode)
+{
+    string errorMsg;
+
+    switch (errCode)
+    {
+    case HOST_NAME_AND_PORT_NO_INVALID:
+        errorMsg = "ERROR: Invalid Host Name and Port No.";
+        break;
+    case SOCKET_CREATION_FAILED:
+        errorMsg = "ERROR: Socket Creation Failed.";
+        break;
+    case CONNECTION_FAILED:
+        errorMsg = "ERROR: Server Connection Failed.";
+        break;
+    case ERROR_SENDING_MSG:
+        errorMsg = "ERROR: Sending Message To Server.";
+        break;
+    case ERROR_READING_MSG:
+        errorMsg = "ERROR: Reading Message From Server.";
+        break;
+    case PORT_NO_INVALID:
+        errorMsg = "ERROR: Invalid Port No.";
+        break;
+    case HOST_NOT_VALID:
+        errorMsg = "ERROR: Host Not Valid.";
+        break;
+
+    default:
+        break;
+    }
+
+    printToConsole(errorMsg);
+
+    exit(1);
+}
+
+/**
+ * @brief Printing console output
+ * @param msg the output to print
+ */
+void printToConsole(string msg)
+{
+    cout << msg << endl;
+}
+
+/**
+ * @brief Starting client process
+ * @param portno the server port no
+ * @param server the server ip
+ */
+void clientProcess(int portno, struct hostent *server)
+{
+    int sockfd;
+    struct sockaddr_in serverAddr;
+
+    // creating socket
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd < 0)
+    {
+        error(SOCKET_CREATION_FAILED);
+    }
+
+    printToConsole("COMPLETED: Server Socket Creation.");
+
+    // intializing serverAddr to NULL
+    bzero((char *)&serverAddr, sizeof(serverAddr));
+
+    // setting serverAddr parameters
+    serverAddr.sin_family = AF_INET;
+    bcopy((char *)server->h_addr, (char *)&serverAddr.sin_addr.s_addr, server->h_length);
+    serverAddr.sin_port = htons(portno);
+
+    // connecting to server
+    if (connect(sockfd, (struct sockaddr *)&serverAddr, sizeof(serverAddr)) < 0)
+    {
+        error(CONNECTION_FAILED);
+    }
+
+    printToConsole("COMPLETED: Connecting To Server.");
+
+    doClientServerCommunications(sockfd);
+}
+
+/**
+ * @brief Client-Server communication
+ * @param sockfd the client socket created for communicating with server
+ */
+void doClientServerCommunications(int sockfd)
+{
+    char *buffer = (char *)malloc(BUFFERSIZE * sizeof(char));
+    char *msg = (char *)malloc(2 * BUFFERSIZE * sizeof(char));
+    char option;
+
+    msg[0] = TYPE1;
+
+    do
+    {
+        memset(buffer, 0, BUFFERSIZE * sizeof(char)); // clearing buffer
+
+        printToConsole("\nType your Message and press enter to send: ");
+
+        fgets(buffer, BUFFERSIZE, stdin); // getting msg from user
+
+        buffer[strcspn(buffer, "\n")] = 0; // removing newline character from input
+
+        doBase64Encoding(buffer, msg); // base 64 encoding
+
+        // sending encoded msg(Type 1) to server
+        if (write(sockfd, msg, strlen(msg)) <= 0)
+        {
+            error(ERROR_SENDING_MSG);
+        }
+
+        memset(buffer, 0, BUFFERSIZE * sizeof(char)); // clearing buffer before reading msg
+
+        // receiving Ack(Type 2) from server
+        if (read(sockfd, buffer, BUFFERSIZE) <= 0)
+        {
+            error(ERROR_READING_MSG);
+        }
+
+        printToConsole("\nNew message from server:");
+
+        printToConsole(buffer);
+
+        printToConsole("\nDo you want to close the connection now?.(Y/N)");
+
+        cin >> option;
+        cin.clear();
+        cin.ignore();
+
+        memset(msg, 0, 2 * BUFFERSIZE * sizeof(char)); // clearing msg
+
+        if (toupper(option) == 'Y')
+        {
+            msg[0] = TYPE3;
+            break;
+        }
+
+        msg[0] = TYPE1;
+    } while (true);
+
+    closeConnectionToServer(sockfd);
+}
+
+/**
+ * @brief Closing connection gracefully
+ * @param sockfd the client socket created for communicating with server
+ */
+void closeConnectionToServer(int sockfd)
+{
+    char *buffer = (char *)malloc(BUFFERSIZE * sizeof(char));
+    char *msg = (char *)malloc(2 * BUFFERSIZE * sizeof(char));
+    clock_t time;
+    ClosingState state;
+
+    memset(msg, 0, 2 * BUFFERSIZE * sizeof(char)); // clearing msg
+    msg[0] = TYPE3;
+
+    // sending Type 3 msg to server
+    strcat(msg, "Finished");
+    if (write(sockfd, msg, strlen(msg)) < 0)
+    {
+        error(ERROR_SENDING_MSG);
+    }
+
+    printToConsole("\nType 3 Msg Send To Server");
+
+    state = FIN_WAIT_1;
+    memset(buffer, 0, BUFFERSIZE * sizeof(char)); // clearing buffer before reading msg
+
+    time = clock();
+    while (state != FIN_WAIT_2)
+    {
+        if (read(sockfd, buffer, BUFFERSIZE) > 0)
+        {
+            // receiving Ack for Type 3 msg from server
+            state = FIN_WAIT_2;
+            printToConsole("CONNECTION CLOSING: ACK Received From Server.");
+        }
+        else if ((clock() - time) > TIMEOUT)
+        {
+            // timeout not received ACK from server
+            state = FIN_WAIT_2;
+            printToConsole("ACK TIMEOUT: Not Received ACK From Server. Entered FIN_WAIT_2 State.");
+        }
+    }
+
+    memset(buffer, 0, BUFFERSIZE * sizeof(char)); // clearing buffer before reading next msg
+    time = clock();
+
+    while ((clock() - time) < TIMEOUT)
+    {
+        if (read(sockfd, buffer, BUFFERSIZE) > 0)
+        {
+            printToConsole("CONNECTION CLOSING: FIN Signal Received From Server.");
+            printToConsole("CONNECTION CLOSING: Last ACK Send To Server.");
+            send(sockfd, ACK, strlen(ACK), 0); // send ACK to server
+            break;
+        }
+    }
+
+    sleep(WAIT_TIME);
+    close(sockfd);
+
+    printToConsole("Connection Closed.");
+}
+
+/**
+ * @brief Base64 encoding
+ * @param msg original message
+ * @param encodedMsg encoded msg to send
+ */
+void doBase64Encoding(char *msg, char *encodedMsg)
+{
+    char base64Chars[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+    int n = strlen(msg);
+    int index, characterCount, numberOfBits, asciiValue, padding;
+    int j, p, k = 1;
+
+    for (int i = 0; i < n; i += 3)
+    {
+        j = i;
+        characterCount = 0;
+        asciiValue = 0;
+
+        while (j < (i + 3) && j < n)
+        {
+            asciiValue = asciiValue << 8;
+            asciiValue = asciiValue | msg[j];
+            characterCount++;
+            j++;
+        }
+
+        numberOfBits = characterCount * 8;
+        padding = numberOfBits % 3;
+
+        while (numberOfBits > 0)
+        {
+            if (numberOfBits > 6)
+            {
+                p = numberOfBits - 6;
+                index = (asciiValue >> p) & 63;
+                numberOfBits -= 6;
+            }
+            else
+            {
+                p = 6 - numberOfBits;
+                index = (asciiValue << p) & 63;
+                numberOfBits = 0;
+            }
+            encodedMsg[k++] = base64Chars[index];
+        }
+    }
+
+    for (j = 0; j < padding; j++)
+    {
+        encodedMsg[k++] = '=';
+    }
+
+    encodedMsg[k] = '\0';
+}
---
+++ Base64_Encoding_System/Server.cpp
@@ -0,0 +1,332 @@
+
+#include <iostream>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+
+using namespace std;
+
+#define BUFFERSIZE 1024
+#define ACK "Message Received"
+#define FIN "Finished"
+#define TYPE1 '1'
+#define TYPE2 '2'
+#define TYPE3 '3'
+#define WAIT_TIME 5  // in seconds
+#define TIMEOUT 3000 // in milli seconds
+
+enum ErrorCode
+{
+    PORT_NO_INVALID,
+    SOCKET_CREATION_FAILED,
+    SOCKET_BINDING_FAILED,
+    CONNECTION_ACCEPTING_FAILED,
+    ERROR_READING_MSG,
+};
+
+void serverProcess(int);
+void listenClientProcesses(int);
+void closeConnectionToClient(int, string);
+void doClientServerCommunications(int, string);
+void doBase64Decoding(char *);
+void error(ErrorCode);
+void printToConsole(string);
+
+int main(int argc, char **argv)
+{
+    int portno;
+    if (argc < 2)
+    {
+        error(PORT_NO_INVALID);
+    }
+
+    portno = atoi(argv[1]); // getting port no as CLI argument
+
+    if (portno < 0 || portno > 65534)
+    {
+        error(PORT_NO_INVALID);
+    }
+
+    serverProcess(portno);
+
+    return 0;
+}
+
+/**
+ * @brief Error handling utility fuction
+ * @param errCode the error code for which exception need to be handled
+ */
+void error(ErrorCode errCode)
+{
+    string errorMsg;
+
+    switch (errCode)
+    {
+    case PORT_NO_INVALID:
+        errorMsg = "ERROR: Invalid Port No.";
+        break;
+    case SOCKET_CREATION_FAILED:
+        errorMsg = "ERROR: Socket Creation Failed.";
+        break;
+    case SOCKET_BINDING_FAILED:
+        errorMsg = "ERROR: Socket Binding Failed.";
+        break;
+    case CONNECTION_ACCEPTING_FAILED:
+        errorMsg = "ERROR: Acccepting Conncetion Failed.";
+        break;
+    case ERROR_READING_MSG:
+        errorMsg = "ERROR: Receiving Message From Client.";
+        break;
+
+    default:
+        break;
+    }
+
+    printToConsole(errorMsg);
+
+    exit(1);
+}
+
+/**
+ * @brief Printing console output
+ * @param msg the output to print
+ */
+void printToConsole(string msg)
+{
+    cout << msg << endl;
+}
+
+/**
+ * @brief Starting server process
+ * @param portno the port no to which server socket to bind
+ */
+void serverProcess(int portno)
+{
+    int sockfd;
+    struct sockaddr_in serverAddr;
+
+    // creating the socket
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd < 0)
+    {
+        error(SOCKET_CREATION_FAILED);
+    }
+
+    printToConsole("COMPLETED: Server Socket Creation.");
+
+    // initializing serverAddr to null value
+    bzero((char *)&serverAddr, sizeof(serverAddr));
+
+    // setting serverAddr parameters
+    serverAddr.sin_family = AF_INET;
+    serverAddr.sin_addr.s_addr = INADDR_ANY;
+    serverAddr.sin_port = htons(portno);
+
+    // binding the socket
+    if (bind(sockfd, (struct sockaddr *)&serverAddr, sizeof(serverAddr)) < 0)
+    {
+        error(SOCKET_BINDING_FAILED);
+    }
+
+    printToConsole("COMPLETED: Server Socket Binding.");
+
+    listen(sockfd, 10); // second arg is queue length
+
+    printToConsole(("\nServer Is Up And Running.\nListening Incoming Connections On Port No " + to_string(portno)));
+
+    listenClientProcesses(sockfd);
+}
+
+/**
+ * @brief Listening incoming connections
+ * @param sockfd the server socket which is listening the client connections
+ */
+void listenClientProcesses(int sockfd)
+{
+    int newsockfd;
+    socklen_t clientlen;
+    struct sockaddr_in clientAddr;
+    string clientIP;
+    pid_t pid;
+
+    do
+    {
+        // accepting new incoming connection
+        clientlen = sizeof(clientAddr);
+        newsockfd = accept(sockfd, (struct sockaddr *)&clientAddr, &clientlen);
+        if (newsockfd < 0)
+        {
+            error(CONNECTION_ACCEPTING_FAILED);
+        }
+        clientIP = inet_ntoa(clientAddr.sin_addr);
+        clientIP = clientIP + '/';
+        clientIP = clientIP + to_string(ntohs(clientAddr.sin_port));
+
+        printToConsole(("\nAccepted New Incoming Connection From Client IP/PortNo: " + clientIP));
+
+        // creating child process for handling client communications
+        if ((pid = fork()) == 0)
+        {
+            // child process
+            close(sockfd); // closing its listening socket
+            doClientServerCommunications(newsockfd, clientIP);
+            exit(0);
+        }
+        close(newsockfd); // parent process closes its connected client socket
+
+    } while (true);
+}
+
+/**
+ * @brief Client-Server communications
+ * @param newsockfd the new socket created for client communications
+ * @param clientIP  the client IP/PortNo
+ */
+void doClientServerCommunications(int newsockfd, string clientIP)
+{
+    char buffer[BUFFERSIZE];
+
+    do
+    {
+        bzero(buffer, sizeof(buffer)); // clearing buffer before reading msg
+
+        if (read(newsockfd, buffer, BUFFERSIZE) <= 0)
+        {
+            printToConsole("Client: " + clientIP);
+            error(ERROR_READING_MSG);
+        }
+
+        if (buffer[0] == TYPE3)
+        {
+            // client has send Type 3 msg
+            printToConsole(("\nClient " + clientIP + " Requested To Close Connection."));
+            break;
+        }
+
+        printToConsole(("\nNew Message Received From Client " + clientIP + "\n\nReceived Message:\n"));
+
+        printToConsole(buffer); // received msg from client
+
+        doBase64Decoding(buffer);
+
+        if (buffer[0] == TYPE1)
+        {
+            // sending ACK to client on recieving Type 1 Msg
+            send(newsockfd, ACK, strlen(ACK), 0);
+        }
+
+    } while (true);
+
+    closeConnectionToClient(newsockfd, clientIP);
+}
+
+/**
+ * @brief Closing conncetion gracefully
+ * @param newsockfd the new socket created for client communications
+ */
+void closeConnectionToClient(int newsockfd, string clientIP)
+{
+    clock_t time;
+    char buffer[BUFFERSIZE];
+    bool receivedAck = false;
+
+    bzero(buffer, sizeof(buffer));        // clearing buffer before reading msg
+    send(newsockfd, ACK, strlen(ACK), 0); // send ACK to client
+    printToConsole("\nCONNECTION CLOSING: ACK Signal Send To Client " + clientIP);
+    sleep(WAIT_TIME);
+    send(newsockfd, FIN, strlen(FIN), 0); // send FIN to client
+    printToConsole("\nCONNECTION CLOSING: FIN Signal Send To Client " + clientIP);
+    time = clock();
+
+    while ((clock() - time) < TIMEOUT)
+    {
+        if (read(newsockfd, buffer, BUFFERSIZE) > 0)
+        {
+            printToConsole("\nCONNECTION CLOSING: Last ACK Received From Client " + clientIP);
+            receivedAck = true;
+            break;
+        }
+    }
+
+    if (!receivedAck)
+        printToConsole("\nACK TIMEOUT: Last ACK From Client " + clientIP + " Not Received.");
+
+    close(newsockfd);
+
+    printToConsole("\nConnection To Client " + clientIP + " Closed.");
+}
+
+/**
+ * @brief Base64 decoding
+ * @param encodedMsg the received msg from client
+ */
+void doBase64Decoding(char *encodedMsg)
+{
+    int n = strlen(encodedMsg);
+    char *decodedMsg = (char *)malloc(BUFFERSIZE * sizeof(char));
+    int asciiValue, numberOfBits;
+    int j, p, k = 0;
+
+    for (int i = 1; i < n; i += 4)
+    {
+        j = i;
+        asciiValue = 0;
+        numberOfBits = 0;
+
+        while (j < (i + 4) && j < n)
+        {
+            if (encodedMsg[j] != '=')
+            {
+                asciiValue = asciiValue << 6;
+                numberOfBits += 6;
+
+                if (encodedMsg[j] >= 'A' && encodedMsg[j] <= 'Z')
+                {
+                    asciiValue = asciiValue | (encodedMsg[j] - 'A');
+                }
+                else if (encodedMsg[j] >= 'a' && encodedMsg[j] <= 'z')
+                {
+                    asciiValue = asciiValue | (encodedMsg[j] - 'a' + 26);
+                }
+                else if (encodedMsg[j] >= '0' && encodedMsg[j] <= '9')
+                {
+                    asciiValue = asciiValue | (encodedMsg[j] - '0' + 52);
+                }
+                else if (encodedMsg[j] == '+')
+                {
+                    asciiValue = asciiValue | 62;
+                }
+                else if (encodedMsg[j] == '/')
+                {
+                    asciiValue = asciiValue | 63;
+                }
+                else
+                {
+                    printToConsole(("ERROR: Unable To Decode " + encodedMsg[j]));
+                }
+            }
+            else
+            {
+                asciiValue = asciiValue >> 2;
+                numberOfBits -= 2;
+            }
+            j++;
+        }
+
+        while (numberOfBits > 0)
+        {
+            p = numberOfBits - 8;
+            decodedMsg[k++] = (asciiValue >> p) & 255;
+            numberOfBits -= 8;
+        }
+    }
+    decodedMsg[k] = '\0';
+
+    printToConsole("\nOriginal Message:\n");
+
+    printToConsole(decodedMsg); // original msg send by client
+}
